b33daded6f9c37c90b1aeca012f5dba2c3ec4025
Константин Колесниченко <kolesnichenko.00@mail.ru>
2019-10-07 14:36:08 +0700

    2.0

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 inp = open("input.txt", "r")
 l1 = []
 l2 = []
+din = [[]]
+def back(x, y, back_l1, back_l2, back_din, s):
+    if x == -1 or y == -1:
+        return s
+    if back_l1[x] == back_l2[y]:
+        s.append(back_l1[x])
+        return back(x-1, y-1, back_l1, back_l2, back_din, s)
+    if back_din[x-1][y] > back_din[x][y-1]:
+        return back(x-1, y, back_l1, back_l2, back_din, s)
+    else:
+        return back(x, y-1, back_l1, back_l2, back_din, s)
 s = inp.readline()
 for i in s:
     if i >= '0' and i <= '9':
@@ -9,7 +20,6 @@ s = inp.readline()
 for i in s:
     if i >= '0' and i <= '9':
         l2.append(int(i))
-din = [[]]
 din.append([])
 if l1[0] == l2[0]:
     din[0].append(1)
@@ -31,6 +41,9 @@ for i in range(1, len(l1)):
             din[i].append(max(din[i-1][j-1]+1, din[i][j-1]))
         else:
             din[i].append(max(din[i-1][j], din[i][j-1]))
+ans = back(len(l1)-1, len(l2)-1, l1, l2, din, [])
+ans.reverse()
 print(l1)
 print(l2)
 print(din[len(l1)-1][len(l2)-1])
+print(ans)
